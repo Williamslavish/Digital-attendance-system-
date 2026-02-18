b090701a5d19e7ac225cdb29673ba0b2765e0945
Williams Paddy - 01242591D <williamslavish719@gmail.com>
2026-02-17 23:45:59 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,63 +1,32 @@
 #include <iostream>
-#include <vector>
-#include "Student.h"
-
+#include<fstream>
 using namespace std;
+int main(){
+}
+// ---------- Create Session ----------
+void createSession() {
+    cout << "Lecture session created successfully!\n\n";
+}
+void searchStudent() {
+    ifstream file("students.txt");
+    string line, searchIndex;
+    bool found = false;
 
-vector<Student> students;
-
-void registerStudent() {
-    string idx, name;
-
-    cout << "Enter Index Number: ";
-    cin >> idx;
+    cout << "Enter index number to search: ";
     cin.ignore();
+    getline(cin, searchIndex);
 
-    cout << "Enter Name: ";
-    getline(cin, name);
-
-    students.push_back(Student(idx, name));
-    cout << "Student registered successfully!\n";
-}
-
-void viewStudents() {
-    cout << "\n--- REGISTERED STUDENTS ---\n";
-    if (students.empty()) {
-        cout << "No students registered.\n";
-        return;
-    }
-
-    for (const auto& s : students) {
-        s.display();
-    }
-}
-
-int main() {
-    int choice;
-
-    do {
-        cout << "\nDIGITAL ATTENDANCE SYSTEM\n";
-        cout << "1. Register Student\n";
-        cout << "2. View Students\n";
-        cout << "0. Exit\n";
-        cout << "Choose: ";
-        cin >> choice;
-
-        switch (choice) {
-        case 1:
-            registerStudent();
-            break;
-        case 2:
-            viewStudents();
-            break;
-        case 0:
-            cout << "Exiting program...\n";
-            break;
-        default:
-            cout << "Invalid choice!\n";
+    while (getline(file, line)) {
+        if (line.find(searchIndex) != string::npos) {
+            cout << "Student Found: " << line << endl;
+            found = true;
         }
-    } while (choice != 0);
+    }
 
-    return 0;
+    if (!found) {
+        cout << "Student not found.\n";
+    }
+
+    file.close();
+    cout << endl;
 }
-
